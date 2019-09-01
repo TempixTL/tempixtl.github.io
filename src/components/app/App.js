cddff584718a5ef534ade3fd3487fa9a76d4493f
TempixTL <tempixtl@icloud.com>
2019-08-31 23:04:18 -0500

    Mostly refactoring

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // Components
 import { Nav } from 'react-bootstrap';
 import FlareComponent from 'flare-react';
@@ -9,36 +9,48 @@ import './App.css';
 import signature from 'assets/signature.flr';
 
 function App() {
-  const [showName, setShowName] = useState(false);
-
-  useEffect(() => {
-    setShowName(true);
-  }, []);
+  const [tab, setTab] = useState("about");
 
   return (
-    <div>
-      <div className="container" style={{padding: "20px 0"}}>
-        <div className="row">
-          <div className="col text-center" style={{width: "200px", height: "200px"}}>
-            <FlareComponent
-              width={200} height={200}
-              animationName="stroke"
-              file={signature} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col text-center">
-            <CSSTransition in={showName} timeout={2000} classNames="name">
-              <h2 className="name">Thomas Lauerman</h2>
-            </CSSTransition>
-          </div>
+    <div className="container">
+      <div className="row">
+        <div className="col text-center" style={{ width: "200px", height: "200px" }}>
+          {/* TODO repeat animation on click */}
+          <FlareComponent
+            width={200} height={200}
+            animationName="stroke"
+            file={signature} />
         </div>
       </div>
-      <Nav variant="pills" defaultActiveKey="about" className="justify-content-center">
+      <div className="row">
+        <div className="col text-center">
+          {/* TODO hide text on initial page load */}
+          <CSSTransition in={true} appear={true} timeout={2000} classNames="name">
+            <h2 className="name">Thomas Lauerman</h2>
+          </CSSTransition>
+        </div>
+      </div>
+      <Nav
+        style={{ paddingTop: "20px" }}
+        variant="pills"
+        defaultActiveKey={tab}
+        className="justify-content-center"
+        onSelect={(eventKey, _) => setTab(eventKey)}>
         <Nav.Link eventKey="about">about</Nav.Link>
         <Nav.Link eventKey="projects">projects</Nav.Link>
+        <Nav.Link eventKey="resume">resume</Nav.Link>
         <Nav.Link eventKey="links">links</Nav.Link>
       </Nav>
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <hr />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <p>{tab}</p>
+        </div>
+      </div>
     </div>
   );
 }
