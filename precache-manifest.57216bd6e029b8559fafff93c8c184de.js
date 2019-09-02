2e8e1a48ac6b82eaf9166ff08b9dd51745930855
TempixTL <tempixtl@icloud.com>
2019-09-01 23:19:15 -0500

    Updates

---
+++ precache-manifest.57216bd6e029b8559fafff93c8c184de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f513dc2f4d235538219c88819cf2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a9c5ca29276cd9e71e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3a398c3173da744e46b6",
+    "url": "/static/css/main.1f58f9d8.chunk.css"
+  },
+  {
+    "revision": "f7a9c5ca29276cd9e71e",
+    "url": "/static/js/2.3cb4d6ca.chunk.js"
+  },
+  {
+    "revision": "3a398c3173da744e46b6",
+    "url": "/static/js/main.dcb2e66b.chunk.js"
+  },
+  {
+    "revision": "38dc52f4397f9580606c",
+    "url": "/static/js/runtime~main.3378c27b.js"
+  },
+  {
+    "revision": "2cb1a5fe32119a2987ca803b2819d482",
+    "url": "/static/media/headshot.2cb1a5fe.jpg"
+  },
+  {
+    "revision": "70b3e8682838de1b2fe2e5b630fff2cc",
+    "url": "/static/media/resume.70b3e868.pdf"
+  },
+  {
+    "revision": "11f8fd770f3b1c915e3fad7efb19c30d",
+    "url": "/static/media/signature.11f8fd77.flr"
+  }
+]);
