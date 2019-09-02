cefb814fb765cb65660c17ec84cb97f94f15a285
TempixTL <tempixtl@icloud.com>
2019-09-02 17:06:41 -0500

    Fixed resume pdf preview on mobile Remove react-pdf

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,26 +1,21 @@
 import React from 'react';
 // Components
-import { pdfjs, Document, Page } from 'react-pdf';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Assets
-import resume from 'assets/resume.pdf';
 import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 
-// * Performance: ensure pdf.worker.js loads
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 function Resume() {
+  const resumePath = process.env.PUBLIC_URL + "/resume.pdf";
+
   return (
     <div className="row justify-content-center">
-      <div className="col-10 text-center">
-        <div className="rounded shadow d-inline-block p-2">
-          <Document file={resume}>
-            <Page pageNumber={1} />
-          </Document>
+      <div className="col-8 text-center">
+        <div className="embed-responsive embed-responsive-1by1">
+          <embed src={resumePath} type="application/pdf" className="embed-responsive-item"/>
         </div>
         <br/>
-        <Button className="m-3" href={resume} target="_blank">
+        <Button className="m-3" href={resumePath} target="_blank">
           <FontAwesomeIcon icon={faFileDownload}/> Download
         </Button>
       </div>
