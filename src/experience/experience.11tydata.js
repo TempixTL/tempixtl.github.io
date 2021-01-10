5c2c60ec134cbe559b7be3a6fbba02578664dd51
Thomas Lauerman <tom@lauerman.dev>
2021-01-10 10:45:21 -0600

    Add experience to data file

---
+++ src/experience/experience.11tydata.js
@@ -0,0 +1,125 @@
+module.data = [
+  {
+    "title": "ACM Head Tutor",
+    "employment_type": "Part-time",
+    "company": "Trinity University",
+    "location": "San Antonio, TX",
+    "start_date": new Date(2019, 0),
+    "end_date": "Present",
+    "description": "TODO",
+    "responsibilities": [
+      "Mentor freshman and sophomore students in foundational Computer Science concepts",
+      "Assist tutees in planning, coding, and debugging multifaceted software projects",
+      "Act as Head Tutor (since January 2020), including hiring and training new tutors",
+    ],
+    "skills": [
+      "scala",
+      "c",
+      "c++",
+      "communication",
+      "problem solving",
+    ],
+  },
+  {
+    "title": "Software Engineer Intern",
+    "employment_type": "Internship",
+    "company": "Google",
+    "location": "Frisco, TX (Remote)",
+    "start_date": new Date(2020, 4),
+    "end_date": new Date(2020, 7),
+    "description": "TODO",
+    "responsibilities": [
+      "Adapting to an unfamiliar tech stack quickly after project change due to pandemic",
+      "Creating a Tensorflow model to predict wether a dish is high or low calorie based on it's name",
+      "Taking initiative to create practical application of the ML model within original timeframe",
+    ],
+    "skills": [
+      "python",
+      "tensorflow",
+      "pandas",
+      "javascript",
+      "chrome extension",
+    ],
+    "links": [
+      {
+        "title": "Project's GitHub Repo",
+        "link": "https://github.com/googleinterns/amaranth",
+      },
+    ],
+  },
+  {
+    "title": "Mobile Developer Intern",
+    "employment_type": "Internship",
+    "company": "GaitIQ",
+    "location": "San Antonio, TX",
+    "start_date": new Date(2019, 4),
+    "end_date": new Date(2019, 6),
+    "description": "TODO",
+    "responsibilities": [
+      "Employed Agile methodology to efficiently communicate with co-workers",
+      "Designed a cross-platform, multi-screen tablet application UI in Sketch",
+      "Transformed design into a fully-functioning React Native application to demo for investors",
+    ],
+    "skills": [
+      "react",
+      "javascript",
+      "sketch",
+      "react native",
+      "typescript",
+    ],
+    "links": [
+      {
+        "title": "GaitIQ Website",
+        "link": "https://gaitiq.com",
+      },
+      {
+        "title": "Trinity's Instagram Post",
+        "link": "https://www.instagram.com/p/BzeWO14l8F1",
+      },
+    ],
+  },
+  {
+    "title": "Palette: colorful puzzles",
+    "employment_type": "Side-project",
+    "company": "Personal Project",
+    "location": "Frisco, TX",
+    "start_date": new Date(2017, 5),
+    "end_date": new Date(2018, 7),
+    "description": "TODO",
+    "responsibilities": [
+      "Developed a 2D, touch-based puzzle game application for iOS written in Swift",
+      "Designed, programmed, and uploaded to the App Store independently",
+      "Researched common app architecture and optimization practices to ensure scalability",
+    ],
+    "skills": [
+      "swift",
+      "iOS",
+      "UIKit",
+      "SpriteKit",
+    ],
+    "links": [
+      {
+        "title": "Palette on the App Store",
+        "link": "https://apps.apple.com/us/app/palette-colorful-puzzles/id1403062573",
+      },
+    ],
+  },
+  {
+    "title": "Salesperson",
+    "employment_type": "Full-time",
+    "company": "CUTCO",
+    "location": "Plano, TX",
+    "start_date": new Date(2016, 6),
+    "end_date": new Date(2017, 1),
+    "description": "TODO",
+    "responsibilities": [
+      "Identified clients, set appointments, and made sales",
+      "Recorded over $6,000 of product sales in one 3-month period",
+      "Acquired professional experience with patience, communication, and presenation skills",
+    ],
+    "skills": [
+      "communication",
+      "presentation skills",
+    ],
+  },
+];
