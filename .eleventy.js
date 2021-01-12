772f04928c6bec029d19e5fef9107e797f48bbba
Thomas Lauerman <tom@lauerman.dev>
2021-01-12 17:27:20 -0600

    Make implicit dependencies tracked by npm

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ const yaml = require("js-yaml");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
+  // npm dependencies
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/normalize.css/normalize.css": "css/normalize.css"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/vanilla-tilt/dist/vanilla-tilt.js": "js/vanilla-tilt.js"
+  });
+
   eleventyConfig.addPassthroughCopy("src/css"); // css styles
   eleventyConfig.addPassthroughCopy("src/img"); // images
   eleventyConfig.addPassthroughCopy("src/js");  // js files
