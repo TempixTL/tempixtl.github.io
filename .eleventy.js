b521be3b45467a5561acf97e18ecf7717b04f89e
Thomas Lauerman <tom@lauerman.dev>
2021-01-12 17:05:31 -0600

    Add yaml support, change experience.json to yaml

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+
   eleventyConfig.addPassthroughCopy("src/css"); // css styles
   eleventyConfig.addPassthroughCopy("src/img"); // images
   eleventyConfig.addPassthroughCopy("src/js");  // js files
