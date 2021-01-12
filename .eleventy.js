25891767741d00e2c9dcd967d16de7a895f4f0a3
Thomas Lauerman <tom@lauerman.dev>
2021-01-11 22:17:18 -0600

    Add vanilla-tilt.js to project

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css"); // css styles
   eleventyConfig.addPassthroughCopy("src/img"); // images
+  eleventyConfig.addPassthroughCopy("src/js");  // js files
   eleventyConfig.addPassthroughCopy("src/dat"); // other data
 
   return {
