4c1887d2cda8b10200fd4a0051d15c7b0fa270bc
Thomas Lauerman <tom@lauerman.dev>
2021-01-14 21:52:34 -0600

    Add serve npm script for development

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,15 @@ const yaml = require("js-yaml");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
+  // gulp-managed dependencies
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/js/");
+  eleventyConfig.addWatchTarget("./src/img/");
+  eleventyConfig.addWatchTarget("./src/dat/");
+
+  // give gulp 500 millis to build before refresh
+  eleventyConfig.setWatchThrottleWaitTime(500);
+
   return {
     dir: {
       input: "src",
